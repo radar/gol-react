88111075880fc499260dc82aa489c60447693ab4
Ryan Bigg <me@ryanbigg.com>
2019-01-29 12:00:23 +1100

    Updates

---
+++ precache-manifest.a36234b0d9b2e8398f9ad539b0203528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce090617967ed1f100c7",
+    "url": "/gol-react/static/js/runtime~main.ce090617.js"
+  },
+  {
+    "revision": "09b9fcaa0df549ab9c1e",
+    "url": "/gol-react/static/js/main.09b9fcaa.chunk.js"
+  },
+  {
+    "revision": "53513036b8933d9e6c9c",
+    "url": "/gol-react/static/js/1.53513036.chunk.js"
+  },
+  {
+    "revision": "09b9fcaa0df549ab9c1e",
+    "url": "/gol-react/static/css/main.ff29f0d3.chunk.css"
+  },
+  {
+    "revision": "bdcc8b9996914daa51a19016d5dd16ca",
+    "url": "/gol-react/index.html"
+  }
+];
